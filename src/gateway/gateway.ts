31248ff105953318c2c49de793ca0d837d475b6c
apple <apple@apples-MacBook-Pro.local>
2023-02-07 18:51:43 +0500

    creating a Websocket gateway server

---
+++ src/gateway/gateway.ts
@@ -0,0 +1,30 @@
+import { OnModuleInit } from '@nestjs/common';
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server } from 'socket.io';
+
+@WebSocketGateway()
+export class MyGateway implements OnModuleInit {
+  @WebSocketServer()
+  server: Server;
+
+  onModuleInit() {
+    this.server.on('connection', (socket) => {
+      console.log(socket.id);
+      console.log('Connected');
+    });
+  }
+
+  @SubscribeMessage('newMessage')
+  onNewMessage(@MessageBody() body: any) {
+    console.log(body);
+    this.server.emit('onMessage', {
+      msg: 'new message',
+      content: body,
+    });
+  }
+}
